da08b74c7b1755a7659dbc52f9f12caea917ba7c
Rodrigo Saavedra <r.saavedra@fz-juelich.de>
2021-07-29 14:44:28 +0200

    Add temperature block

--- config.def.h
+++ config.def.h
@@ -4,12 +4,13 @@ static const Block blocks[] = {
 	/* {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0}, */
 
 	/* {"", "date '+%b %d (%a) %I:%M%p'",					5,		0}, */
-    {"",   "sb-mpc",      0,     11},
-    {"",   "sb-weather",  18000, 5},
-    {"",   "sb-memory",   10,    14},
-    {"",   "sb-kbselect", 0,     30},
-    {"",   "sb-clock",    60,    1},
-    {"",   "sb-battery",  5,     3},
+    {"", "sb-mpc",         0,     11},
+    {"", "sb-weather",     18000, 5},
+    {"", "sb-temperature", 0,     22},
+    {"", "sb-memory",      10,    14},
+    {"", "sb-kbselect",    0,     30},
+    {"", "sb-clock",       60,    1},
+    {"", "sb-battery",     5,     3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
