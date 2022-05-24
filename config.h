6b23ee6ece7ab79bbf4b9a09de203852a687d7d2
Rodrigo Saavedra <r.saavedra@fz-juelich.de>
2022-05-24 21:40:14 +0200

    Minimal statusbar
    Only essential keyboard+battery+calendar

--- config.h
+++ config.h
@@ -1,15 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/  /*Command*/        /*Update-Interval*/  /*Update-Signal*/
-    {"",  "sb-kaffeine",       0,   4},
+    {"",  "sb-blank",          0,   2},
+    {"",  "sb-monitor",        0,   13},
     {"",  "sb-notifications",  0,   5},
+    {"",  "sb-kaffeine",       0,   4},
     {"",  "sb-mpc",            0,   11},
-    {"",  "sb-sysmon",         60,  14},
     {"",  "sb-kbselect",       0,   30},
-    {"",  "sb-volume",         0,   10},
+    {"",  "sb-battery",        60,  3},
     {"",  "sb-calendar",       0,   7},
-    {"",  "sb-clock",          30,  1},
-    {"",  "sb-battery",        60,   3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
